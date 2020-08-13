31498641c2d047c47206651b6184b3d690aeff19
Shawn Stawiarski <contact@shawnstawiarski.com>
2020-08-13 13:04:29 -0400

    Fix navbar links to direct to homepage

--- src/components/Navigation.mjs
+++ src/components/Navigation.mjs
@@ -35,11 +35,11 @@ class Navigation extends React.Component {
         return (
             <div id="navbar">
                 <Navbar bg={this.props.bg} expand="lg">
-                    <Navbar.Brand href="/home">MovieDB</Navbar.Brand>
+                    <Navbar.Brand href="/">MovieDB</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link href="/home">Home</Nav.Link>
+                            <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link href="/movies">Movies</Nav.Link>
                             <Nav.Link href="/about">About</Nav.Link>
                         </Nav>
