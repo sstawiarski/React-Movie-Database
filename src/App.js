d52185e6dc06f997e63acce1a20ee3080edb4534
Shawn Stawiarski <contact@shawnstawiarski.com>
2020-08-13 13:04:15 -0400

    Move BrowserRouter to index.js and update App.js

--- src/App.js
+++ src/App.js
@@ -3,34 +3,34 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navigation } from './components/Navigation';
 import Container from 'react-bootstrap/Container'
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Movies from './components/Movies';
-import MovieSearch from './components/MovieSearch';
+import MovieDetails from './components/MovieDetails';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTerm: ""
+      searchTerm: "",
+      isSearching: false
     }
   }
 
   render() {
     return (
-      <BrowserRouter>
         <div className="App">
           <Container>
-            <Navigation bg="light" handleSearch={(term)=> this.setState({searchTerm: term})} />
+            <Navigation bg="light" handleSearch={(term)=> this.setState({searchTerm: term, isSearching: true})} />
             <Switch>
-              <Route path="/home" component={Home} />
+              <Route exact path="/" component={Home} />
               <Route path="/movies" component={Movies} />
-              <Route path="/details/:title" component={MovieSearch}/>
+              <Route path="/details/:title" component={MovieDetails}/>
             </Switch>
-            {/*this.state.searchTerm && <MovieSearch input={this.state.searchTerm}/>*/}
+            {this.state.searchTerm && alert(this.state.searchTerm)}
+            {this.state.searchTerm && this.state.isSearching && <MovieDetails input={this.state.searchTerm}/>}
           </Container>
         </div>
-      </BrowserRouter>
     );
   }
 
