4eea53fb9bbc5d836c35341a83bc1022c32caade
Shawn Stawiarski <contact@shawnstawiarski.com>
2020-08-13 15:31:37 -0400

    Move submit handling into child component

--- src/components/Home.mjs
+++ src/components/Home.mjs
@@ -6,19 +6,6 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            featured: null,
-        }
-
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleSubmit(text) {
-        this.setState({featured: text});
-    }
-
     render() {
         return (
             <div id="main-content">
@@ -29,8 +16,8 @@ class Home extends React.Component {
                     </Col>
 
                     <Col xs={4}>
-                        <Featured key={Date.now()} bg="light" featured={this.state.featured}/>
-                        <FeaturedEditor onSubmit={this.handleSubmit}/>
+                        <Featured key={Date.now()} bg="light" />
+                        <FeaturedEditor />
                     </Col>
 
                 </Row>
