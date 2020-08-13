eee088423347c2af1ba787ac829126473237d21a
Shawn Stawiarski <contact@shawnstawiarski.com>
2020-08-13 12:50:07 -0400

    Initial setup of searchbar for movies page

---
+++ src/components/Searchbar.mjs
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Searchbar extends React.Component {
+
+    render() {
+        return (
+            <div id="searchbar">
+                <input type="text"></input>
+            </div>
+        );
+    }
+}
+
+export { Searchbar };
