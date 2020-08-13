c4f3df91d77d26541d708462dc68ec1fa73a6c59
Shawn Stawiarski <contact@shawnstawiarski.com>
2020-08-13 16:23:35 -0400

    Fix formatting on mobile

--- src/components/Home.mjs
+++ src/components/Home.mjs
@@ -1,29 +1,30 @@
 import React from 'react';
 import { Body } from "./Body";
 import { Featured, FeaturedEditor } from './Featured';
+import { PostCreator } from './PostCreator'
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container'
 
 class Home extends React.Component {
     render() {
         return (
-            <div id="main-content">
+            <Container id="main-content">
                 <Row>
-
-                    <Col>
+                    <Col xs={12} md={8}>
                         <Body />
                     </Col>
 
-                    <Col xs={4}>
+                    <Col xs={12} md={4}>
                         <Featured key={Date.now()} bg="light" />
                         <FeaturedEditor />
                     </Col>
-
                 </Row>
-
-
-            </div>
+                <Row>
+                    <PostCreator/>
+                </Row>
+            </Container>
 
         );
     }
