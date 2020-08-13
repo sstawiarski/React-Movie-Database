e3e7d6adc6a3180cd53224aa8e002bb470875492
Shawn Stawiarski <contact@shawnstawiarski.com>
2020-08-13 18:30:30 -0400

    Implement searchbar component for movie search

--- src/components/Searchbar.mjs
+++ src/components/Searchbar.mjs
@@ -1,12 +1,39 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form';
 
 class Searchbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        event.preventDefault();
+        this.setState({value: event.target.value})
+        this.props.onChange(this.state.value);
+    }
 
     render() {
         return (
-            <div id="searchbar">
-                <input type="text"></input>
-            </div>
+            <Container id="searchbar" style={{width: "85%", marginTop: "5%"}}>
+                <Form>
+                    <Form.Group controlId="database-search">
+                        <Form.Control type="text" value={this.state.value} onChange={this.handleChange} />
+                        <Form.Text className="text-muted">
+                            Enter a movie title here to search the database.
+                        </Form.Text>
+                    </Form.Group>
+                </Form>
+            </Container>
         );
     }
 }
