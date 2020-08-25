27858408df854980d4cc56018b80edc297d4c77c
Shawn Stawiarski <contact@shawnstawiarski.com>
2020-08-25 18:57:45 -0400

    Update movie details to support title/id search

--- src/components/MovieDetails.mjs
+++ src/components/MovieDetails.mjs
@@ -10,81 +10,62 @@ class MovieDetails extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            searchTerm: decodeURI(this.props.match.params.title),
+            searchTerm: this.props.match.params.id,
             isFound: null,
             title: null,
             year: null,
             poster: null,
             plot: null,
-            director: null,
-            genre: null,
-            actors: null,
             imdb: null,
-            isMounted: false
         }
 
-        this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidMount() {
         this.setState({ isMounted: true });
-        fetch(`http://www.omdbapi.com/?apikey=cc276c76&t=${this.state.searchTerm}`)
+
+        if (!this.props.isText) {
+            fetch(`http://localhost:4000/moviedetails/${this.state.searchTerm}`)
             .then(res => res.json())
             .then(json => {
                 if (this.state.isMounted) {
                     this.setState({
-                        title: json.Title,
-                        year: json.Year,
-                        plot: json.Plot,
-                        poster: json.Poster,
-                        director: json.Director,
-                        genre: json.Genre,
-                        actors: json.Actors.split(", "),
+                        title: json.title,
+                        year: json.year,
+                        plot: json.plot,
+                        poster: json.poster,
                         isFound: true,
                         searchTerm: "",
-                        imdb: json.imdbID
+                        imdb: json.imdb
                     });
                 }
             })
             .catch(err => console.log(err));
+            
+        } else {
+            fetch(`http://localhost:4000/movielist/${this.props.location.state.title}`)
+            .then(res => res.json())
+            .then(json => {
+                json = json[0];
+                if (this.state.isMounted) {
+                    this.setState({
+                        title: json.title,
+                        year: json.year,
+                        plot: json.plot,
+                        poster: json.poster,
+                        isFound: true,
+                        searchTerm: "",
+                        imdb: json.imdb
+                    });
+                }
+            })
+            .catch(err => console.log(err));
+        }
+        
     }
 
     componentWillUnmount() {
         this.setState({ isMounted: false });
-    }
-
-    handleClick(event) {
-        event.preventDefault();
-        if (!this.state.title) {
-            return;
-        }
-
-        try {
-            let newMovie = {
-                imdb: this.state.imdb,
-                title: this.state.title,
-                year: this.state.year,
-                plot: this.state.plot
-            };
-            fetch("http://localhost:4000/movielist", {
-                method: "POST",
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(newMovie)
-            }).then(res => res.json())
-                .then((json) => {
-                    if (json.success) {
-                        alert(this.state.title + " added to the local database.");
-                    } else {
-                        alert("Could not create new post");
-                    }
-                })
-
-        } catch {
-            alert("Error adding to database");
-        }
     }
 
     render() {
@@ -101,22 +82,6 @@ class MovieDetails extends React.Component {
                                     <p>{this.state.plot}</p>
                                     <br />
 
-                                    <h4 id="details-heading"><b>Directed By</b></h4>
-                                    <p>{this.state.director}</p>
-                                    <br />
-
-                                    <h4 id="details-heading"><b>Castlist</b></h4>
-                                    <Table striped bordered>
-                                        <tbody>
-                                            {this.state.actors && this.state.actors.map(actor =>
-                                                <tr key={actor}>
-                                                    <td>{actor}</td>
-                                                </tr>
-                                            )}
-
-                                        </tbody>
-                                    </Table>
-
                                 </Card.Body>
                             </Card>
                         </Col>
@@ -125,16 +90,11 @@ class MovieDetails extends React.Component {
                             <Card bg="light">
                                 <Card.Header>Quick Info</Card.Header>
                                 <Card.Body>
-                                    <Card.Img src={this.state.poster} id="featured-movie-poster" />
+                                    <Card.Img src={this.state.poster} className="featured-movie-poster" />
                                     <br />
-                                    <p><b>Directed by: </b> {this.state.director}</p>
                                     <p><b>Description: </b> {this.state.plot}</p>
-                                    <p><b>Genre: </b> {this.state.genre}</p>
                                     <br />
                                     <br />
-                                    <div id="quick-info-btn-holder">
-                                        <Button variant="primary" id="quick-info-btn" onClick={this.handleClick}>Add movie to local database</Button>
-                                    </div>
 
                                 </Card.Body>
                             </Card>
