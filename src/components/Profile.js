986220194d39152542d1a4ccad779e310d6c7146
Shawn Stawiarski <contact@shawnstawiarski.com>
2020-08-26 18:54:07 -0400

    Add support for UserContext

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as UserLogo } from '../assets/user-male-circle.svg'
 import { auth } from '../firebase/firebase.utils'
+import UserContext from '../UserContext'
 
-const Profile = ({ currentUser }) => (
-    <div className="profile">
-        {
-            currentUser ?
-            <Link className="profile-text" onClick={() => auth.signOut()}>Sign Out</Link>
-            :
-            <Link to="/signin" className="profile-text">Sign In</Link>
-        }
-        <UserLogo className="profile-image" />
-    </div>
-);
+const Profile = () => {
+    const context = React.useContext(UserContext);
+    const currentUser = context.user;
+    return (
+        <div className="profile">
+            {
+                currentUser ?
+                    <Link className="profile-text" onClick={() => auth.signOut()}>Sign Out</Link>
+                    :
+                    <Link to="/signin" className="profile-text">Sign In</Link>
+            }
+            <UserLogo className="profile-image" />
+        </div>
+    )
+};
 
 export default Profile;
