699a64f00e3d9c17d8f25478cd30a53756c3298e
Shawn Stawiarski <contact@shawnstawiarski.com>
2020-09-18 15:15:33 -0400

    Add styling to post creator box

--- src/pages/ForumThread.js
+++ src/pages/ForumThread.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import Container from 'react-bootstrap/Container';
-import ForumPost from  '../components/ForumPost';
+import ForumPost from '../components/ForumPost';
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
-import { store } from '../authentication/UserProvider';
+import {store} from '../authentication/UserProvider';
 
 const ForumThread = (props) => {
     const [posts, setPosts] = React.useState(null);
@@ -17,10 +17,10 @@ const ForumThread = (props) => {
 
     React.useEffect(() => {
         fetch(`http://localhost:4000/forums/${props.match.params.id}/${props.match.params.threadId}`)
-        .then(response => response.json())
-        .then(json => {
-            if (json.postsFound) setPosts(json.posts);
-        })
+            .then(response => response.json())
+            .then(json => {
+                if (json.postsFound) setPosts(json.posts);
+            })
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [postSuccess])
 
@@ -55,32 +55,42 @@ const ForumThread = (props) => {
 
     return (
         <Container>
-            {posts ? 
-            <div>
-                {posts.map(item => {
-                    return (
-                        <ForumPost key={item._id}
-                        postId={item._id}
-                        postContent={item.postContent}
-                        postCreator={item.postCreator}
-                        dateCreated={new Date(item.dateCreated).toLocaleDateString()}
-                        dateUpdated={item.dateUpdated}
-                        editedBy={item.editedBy} />
-                    );
-                })}
-            </div> 
-            : null}
-            {userState.state.user ? <Button variant="link" style={{display: "block", textAlign: "center", marginBottom: "10px", marginLeft: "auto", marginRight: "auto"}} onClick={() => setCreatePost(!createPost)}>[Create new post]</Button> : null}
-            {createPost ?
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group controlId="forumName">
-                        <Form.Label>Forum name:</Form.Label>
-                        <Form.Control as={"textarea"} rows={5} onChange={handleChange} />
-                    </Form.Group>
-                    <Button type="submit" variant="primary">Submit</Button>
-                    <hr />
-                </Form>
+            {posts ?
+                <div>
+                    {posts.map(item => {
+                        return (
+                            <ForumPost key={item._id}
+                                       postId={item._id}
+                                       postContent={item.postContent}
+                                       postCreator={item.postCreator}
+                                       dateCreated={new Date(item.dateCreated).toLocaleDateString()}
+                                       dateUpdated={item.dateUpdated}
+                                       editedBy={item.editedBy}/>
+                        );
+                    })}
+                </div>
                 : null}
+            {userState.state.user ? <Button variant="link" style={{
+                display: "block",
+                textAlign: "center",
+                marginBottom: "10px",
+                marginLeft: "auto",
+                marginRight: "auto"
+            }} onClick={() => setCreatePost(!createPost)}>[Create new post]</Button> : null}
+            {createPost ?
+                <div>
+                    <hr/>
+
+                    <Form onSubmit={handleSubmit} style={{width: "80%", margin: "10px auto 0 auto"}}>
+                        <Form.Group controlId="postContent">
+                            <Form.Label>Post content:</Form.Label>
+                            <Form.Control as={"textarea"} rows={5} onChange={handleChange}/>
+                        </Form.Group>
+                        <Button type="submit" variant="primary">Submit</Button>
+                    </Form>
+                </div>
+                : null}
+
         </Container>
     );
 }
