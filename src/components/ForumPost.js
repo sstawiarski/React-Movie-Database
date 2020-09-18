057af75e7eb86727bd9fc1e48b502cf563dc1a66
Shawn Stawiarski <contact@shawnstawiarski.com>
2020-09-18 15:08:27 -0400

    Add styling to forum posts

--- src/components/ForumPost.js
+++ src/components/ForumPost.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import { HashLink as Link } from 'react-router-hash-link';
-
 import Card from 'react-bootstrap/Card';
 
 const ForumPost = ({ postId, postContent, postCreator, dateCreated, dateUpdated, editedBy }) => {
 
     const shortPostId = postId.substr(0, 7) + '...';
     return (
-        <Card bg="light" id={postId}>
+        <Card bg="light" className={"forum-post"} id={postId}>
             <Card.Header><Link to={`#${postId}`}>#{`${shortPostId}`}</Link>{`  ${postCreator}`}<span style={{ float: "right" }}>{`${dateCreated}`}</span></Card.Header>
             <Card.Body>
                 {postContent}
