632651b290cbac165a73ba6bbdb6bb532288cb2d
Shawn Stawiarski <contact@shawnstawiarski.com>
2020-09-18 15:09:16 -0400

    Add ability to create new post in thread

--- src/pages/ForumThread.js
+++ src/pages/ForumThread.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 import Container from 'react-bootstrap/Container';
 import ForumPost from  '../components/ForumPost';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+import { store } from '../authentication/UserProvider';
 
 const ForumThread = (props) => {
     const [posts, setPosts] = React.useState(null);
+    const [createPost, setCreatePost] = React.useState(false);
+    const [postBody, setPostBody] = React.useState('');
+
+    const userState = React.useContext(store);
 
     React.useEffect(() => {
         fetch(`http://localhost:4000/forums/${props.match.params.id}/${props.match.params.threadId}`)
@@ -14,6 +22,34 @@ const ForumThread = (props) => {
         })
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const body = {
+            postContent: postBody
+        }
+
+        fetch(`http://localhost:4000/forums/${props.match.params.id}/${props.match.params.threadId}`, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            credentials: 'include',
+            body: JSON.stringify(body)
+        })
+            .then(response => response.json())
+            .then(json => {
+                if (json.success) {
+                    setCreatePost(!createPost);
+                    setPostBody('');
+                }
+            })
+    }
+
+    const handleChange = (event) => {
+        setPostBody(event.target.value)
+    }
 
     return (
         <Container>
@@ -32,6 +68,17 @@ const ForumThread = (props) => {
                 })}
             </div> 
             : null}
+            {userState.state.user ? <Button variant="link" style={{display: "block", textAlign: "center", marginBottom: "10px", marginLeft: "auto", marginRight: "auto"}} onClick={() => setCreatePost(!createPost)}>[Create new post]</Button> : null}
+            {createPost ?
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="forumName">
+                        <Form.Label>Forum name:</Form.Label>
+                        <Form.Control as={"textarea"} rows={5} onChange={handleChange} />
+                    </Form.Group>
+                    <Button type="submit" variant="primary">Submit</Button>
+                    <hr />
+                </Form>
+                : null}
         </Container>
     );
 }
